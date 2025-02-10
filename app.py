0585438575b77c82347985ce0588d18618bf4581
shamanth-acharya <shamanthacharya67@gmail.com>
2025-02-10 07:32:45 +0000

    added app.py file

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return "Hello, Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
