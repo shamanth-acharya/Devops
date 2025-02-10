1b35fb397630fd8fa718009b4e0129f598e3172d
shamanth-acharya <shamanthacharya67@gmail.com>
2025-02-10 17:02:56 +0000

    changed all code

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-# app.py
 from flask import Flask
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def home():
-    return "Hello, Docker!"
+    return "Hello, World! Your Flask app is running on GKE."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
